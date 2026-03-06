7226868d013bacfc46988572e06f1fde2cb1b5c2
Ignorant12321 <2185018561@qq.com>
2026-03-06 22:59:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ export const siteConfig: SiteConfig = {
 	title: "Ignorant's Blog",
 	subtitle: "Ignorant 的个人博客",
 	siteURL: "https://ignorant.top/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2026-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-02-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
