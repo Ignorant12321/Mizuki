721a8ba941d984b5e82ec76b854cc9fec7d01df8
Ignorant <2185018561@qq.com>
2026-04-03 18:33:14 +0800

    PageHeader

--- src/components/features/page-header/types.ts
+++ src/components/features/page-header/types.ts
@@ -2,4 +2,8 @@ export interface PageHeaderProps {
 	title: string;
 	subtitle?: string;
 	class?: string;
+	watermark?: string | boolean;
+	bgImage?: string;
+	desc?: string;
+	divider?: boolean;
 }
